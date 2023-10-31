5e5d57eff3bf33c07d25e11b550576c77cc331fe
Vighnesh Ch <94525514+vighnesh-1@users.noreply.github.com>
2023-10-31 12:02:38 +0530

    Add files via upload

---
+++ Data/electronicProduct.js
@@ -0,0 +1,58 @@
+export const electronicsProductData = [
+    {
+        img : "https://rukminim1.flixcart.com/image/200/200/kmp7ngw0/monitor/j/z/h/s2721hn-27-py0df-dell-original-imagfjphuywuh2a7.jpeg?q=70",
+        productName : "monitor",
+        discount : "from ₹8279",
+        brand : "dell",
+        link : "#"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/200/200/l5jxt3k0/dslr-camera/m/n/a/-original-imagg7hsggshhwbz.jpeg?q=70",
+        productName : "top mirrorless cameras",
+        discount : "Shop Now!",
+        brand : "canon, sony, fujifilm...",
+        link : "#"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
+        productName : "printers",
+        discount : "from ₹3999",
+        brand : "hp",
+        link : "#"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/200/200/kmp7ngw0/monitor/j/z/h/s2721hn-27-py0df-dell-original-imagfjphuywuh2a7.jpeg?q=70",
+        productName : "monitor",
+        discount : "from ₹8279",
+        brand : "dell",
+        link : "#"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/400/400/ks7tuvk0/gimbal/v/z/7/0-43-om4-se-dji-original-imag5u6rgsdh6k4g.jpeg?q=70",
+        productName : "monitor",
+        discount : "from ₹8279",
+        brand : "dell",
+        link : "#"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/400/400/kingqkw0-0/hair-straightener/s/v/x/hp8302-06-philips-original-imaf9jzzzurgqykt.jpeg?q=70",
+        productName : "best of hair straighteners",
+        discount : "from ₹8279",
+        brand : "philips",
+        link : "#"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/200/200/kmp7ngw0/monitor/j/z/h/s2721hn-27-py0df-dell-original-imagfjphuywuh2a7.jpeg?q=70",
+        productName : "monitor",
+        discount : "from ₹8279",
+        brand : "dell",
+        link : "#"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/image/200/200/kmp7ngw0/monitor/j/z/h/s2721hn-27-py0df-dell-original-imagfjphuywuh2a7.jpeg?q=70",
+        productName : "monitor",
+        discount : "from ₹8279",
+        brand : "dell",
+        link : "#"
+    },
+]
